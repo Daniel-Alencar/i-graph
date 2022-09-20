d115f6336721c4dfbc23eb75072a7f97c55588b6
Daniel-Alencar <danielalencar746@gmail.com>
2022-09-20 13:14:26 -0300

    organizando algumas coisas

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,16 +13,20 @@ import Calculator from './latex';
 import * as dat from 'dat.gui';
 import { operation_R1_I, operation_R2_R } from './functions';
 
+// constants
 const step = 0.01;
+const axesLength = 15;
 
+// Interface auxiliar
 const gui = new dat.GUI();
 const options = {
-  cameraPositionX: -10, 
-  cameraPositionY: 40, 
-  cameraPositionZ: 30,
-  index_variation: 0 
+  index_variation: 0,
+  cameraPositionX: -10,
+  cameraPositionY: +40,
+  cameraPositionZ: +30,
 };
 
+// Configurações padrões
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -41,74 +45,142 @@ camera.lookAt(new THREE.Vector3(0, 15, 0));
 camera.position.set(
   options.cameraPositionX,
   options.cameraPositionY,
-  options.cameraPositionZ
+  options.cameraPositionZ,
 );
-
-// const cameraHelper = new THREE.CameraHelper(camera);
-// scene.add(cameraHelper);
-
-// gui.add(options, 'cameraPositionX').onChange((property) => {
-//   camera.position.x = property;
-// });
-// gui.add(options, 'cameraPositionY').onChange((property) => {
-//   camera.position.y = property;
-// });
-// gui.add(options, 'cameraPositionZ').onChange((property) => {
-//   camera.position.z = property;
-// });
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-
-
-
-
-
-const axesLength = 15;
-// Define the axis X, Y and Z
-const axesHelper = new THREE.AxesHelper(axesLength);
-scene.add(axesHelper);
+// Define os eixos X, Y e Z
+// const axesHelper = new THREE.AxesHelper(axesLength);
+// scene.add(axesHelper);
 
 const materialsForAxes = [
   new THREE.LineBasicMaterial({
     color: 0xff0000,
-    linewidth: 1
+    linewidth: 2
   }),
   new THREE.LineBasicMaterial({
     color: 0x00ff00,
-    linewidth: 1
+    linewidth: 2
   }),
   new THREE.LineBasicMaterial({
     color: 0x0000ff,
-    linewidth: 1
+    linewidth: 2
   })
 ]
 const pointsForAxes = [
   new THREE.Vector3(-axesLength, 0, 0),
   new THREE.Vector3(0, -axesLength, 0),
-  new THREE.Vector3(0, 0, -axesLength)
+  new THREE.Vector3(0, 0, -axesLength),
+];
+const pointsForAxesEnd = [
+  new THREE.Vector3(+axesLength, 0, 0),
+  new THREE.Vector3(0, +axesLength, 0),
+  new THREE.Vector3(0, 0, +axesLength),
 ];
 
 for(let i = 0; i < pointsForAxes.length; i++) {
   const negativeAxeGeometry = new THREE.BufferGeometry();
-  negativeAxeGeometry.setFromPoints([new THREE.Vector3(0,0,0), pointsForAxes[i]]);
+  negativeAxeGeometry.setFromPoints([pointsForAxesEnd[i], pointsForAxes[i]]);
   
   const negativeAxe = new THREE.Line(negativeAxeGeometry, materialsForAxes[i]);
   scene.add(negativeAxe);
 }
 orbit.update();
 
+// Define o plano de orientação dos eixos
+// const planeGeometry = new THREE.PlaneGeometry(30, 30); 
+// const planeMaterial = new THREE.MeshBasicMaterial({ 
+//   color: 0xff00ff, 
+//   side: THREE.DoubleSide,
+// });
+// const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// scene.add(plane);
+// plane.rotation.x = 0.5 * Math.PI;
+
+// Faz o GRID no plano
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+// Define a criação do texto
+let text0, text1, text2;
+const textMaterial0 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const textMaterial1 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const textMaterial2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+let firstTime = false;
+
+function createText(font) {
+  if(firstTime === false) {
+
+    for(let value = -axesLength; value <= axesLength; value++) {
+      let textGeometry = new TextGeometry(value.toString(), {
+        size: 0.3,
+        height: 0,
+        font: font,
+      });
+
+      text0 = new THREE.Mesh(textGeometry, textMaterial0);
+      text0.position.set(value, 0, 0);
+      text0.lookAt(camera.position);
+      scene.add(text0);
+
+      text1 = new THREE.Mesh(textGeometry, textMaterial1);
+      text1.position.set(0, value, 0);
+      text1.lookAt(camera.position);
+      scene.add(text1);
+
+      text2 = new THREE.Mesh(textGeometry, textMaterial2);
+      text2.position.set(0, 0, value);
+      text2.lookAt(camera.position);
+      scene.add(text2);
+    }
+
+    firstTime = true;
+  } else {
+    text0.lookAt(camera.position);
+    text1.lookAt(camera.position);
+    text2.lookAt(camera.position);
+  }
+}
+
+// Define a fonte
+let font;
+const fontURL = "./droid_serif_regular.json";
+const fontLoader = new FontLoader();
+fontLoader.load(
+  fontURL,
+  function(droidFont) {
+    font = droidFont;
+  }
+);
+
+// Define um BOX de referência
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
 
 
-
-
-
+// Define a luz ambiente da cena
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 ambientLight.position.set(0, 0, 0);
 
+// Define uma luz direcional na cena 
+// const directionLightHelper = new THREE.DirectionalLightHelper(ambientLight, 5);
+// scene.add(directionLightHelper);
 
 
+
+
+
+
+
+
+
+addEventListener('click', () => {
+  console.log(camera.position);
+});
 
 
 
@@ -119,14 +191,12 @@ ambientLight.position.set(0, 0, 0);
 
 
 // Equations
-const graphMaterial = new THREE.LineBasicMaterial({
-  color: 0xf3f6f4,
-  linewidth: 1
-});
-
+// const graphMaterial = new THREE.LineBasicMaterial({
+//   color: 0xf3f6f4,
+//   linewidth: 1
+// });
 
 // let points0 = [];
-
 // f: R -> R
 // f: R -> I
 
@@ -134,9 +204,9 @@ const graphMaterial = new THREE.LineBasicMaterial({
 //   let result = operation_R1_I(x);
 
 //   if(typeof(result) == "object") {
-//     points0.push(new THREE.Vector3(x, result.re, result.im));
-//   } else {
-//     points0.push(new THREE.Vector3(x, result, 0));
+  //     points0.push(new THREE.Vector3(x, result.re, result.im));
+  //   } else {
+    //     points0.push(new THREE.Vector3(x, result, 0));
 //   }
 // }
 
@@ -147,7 +217,6 @@ const graphMaterial = new THREE.LineBasicMaterial({
 
 
 // f: R2 -> R
-
 // for(let x = -axesLength; x <= axesLength; x = x + step) {
 //   for(let y = -axesLength; y <= axesLength; y = y + step) {
 
@@ -165,103 +234,49 @@ const graphMaterial = new THREE.LineBasicMaterial({
 
 
 // f: R2 -> I
+// let points = [];
+// for(let x = -axesLength; x <= axesLength; x = x + step) {
+//   for(let y = -axesLength, i = 0; y <= axesLength; y = y + step, i++) {
 
-
-let points = [];
-for(let x = -axesLength; x <= axesLength; x = x + step) {
-  for(let y = -axesLength, i = 0; y <= axesLength; y = y + step, i++) {
-
-    // Primeira iteração do FOR da variável X 
-    if(x === -axesLength) {
-      points.push(y);
-      points[i] = [];
-    }
+//     // Primeira iteração do FOR da variável X 
+//     if(x === -axesLength) {
+//       points.push(y);
+//       points[i] = [];
+//     }
     
-    let result = operation_R2_R(x, y);
-    if(typeof(result) == "number") {
-      points[i].push(new THREE.Vector3(x, result, 0));
-    } else {
-      points[i].push(new THREE.Vector3(x, result.re, result.im));
-    }
-  }
-}
+//     let result = operation_R2_R(x, y);
+//     if(typeof(result) == "number") {
+//       points[i].push(new THREE.Vector3(x, result, 0));
+//     } else {
+//       points[i].push(new THREE.Vector3(x, result.re, result.im));
+//     }
+//   }
+// }
 
-let indexVariationComponent = gui.add(options, 'index_variation', 0, points.length - 1, 1);
-indexVariationComponent.onChange((property) => {
-  graphGeometry.setFromPoints(points[property]);
-});
-
-const graphGeometry = new THREE.BufferGeometry().setFromPoints(points[options.index_variation]);
-const graph = new THREE.Line(graphGeometry, graphMaterial);
-scene.add(graph);
-
-
-
-// const directionLightHelper = new THREE.DirectionalLightHelper(ambientLight, 5);
-// scene.add(directionLightHelper);
-
-// const planeGeometry = new THREE.PlaneGeometry(30, 30); 
-// const planeMaterial = new THREE.MeshBasicMaterial({ 
-//   color: 0xffffff, 
-//   side: THREE.DoubleSide,
-// });
-// const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-// scene.add(plane);
-// plane.rotation.x = 0.5 * Math.PI;
-
-
-// const material = new THREE.LineBasicMaterial({
-// 	color: 0x0000ff
+// let indexVariationComponent = gui.add(options, 'index_variation', 0, points.length - 1, 1);
+// indexVariationComponent.onChange((property) => {
+//   graphGeometry.setFromPoints(points[property]);
 // });
 
-// const points = [];
-// points.push( new THREE.Vector3(0, 0, 0) );
-// points.push( new THREE.Vector3(0, 10, 0) );
-// points.push( new THREE.Vector3(10, 10, 0) );
-
-// const geometry = new THREE.BufferGeometry().setFromPoints( points );
-// const line = new THREE.Line( geometry, material );
-// scene.add( line );
-
-let font;
-const fontURL = "./droid_serif_regular.json";
-const fontLoader = new FontLoader();
-fontLoader.load(
-  fontURL,
-  function(droidFont) {
-    font = droidFont;
-
-    for(let value = -axesLength; value <= axesLength; value++) {
-      let textGeometry = new TextGeometry(value.toString(), {
-        size: 0.3,
-        height: 0,
-        font: droidFont,
-      });
-
-      let textMaterial0 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-      let text0 = new THREE.Mesh(textGeometry, textMaterial0);
-      text0.position.set(value, 0, 0);
-      scene.add(text0);
-
-      let textMaterial1 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      let text1 = new THREE.Mesh(textGeometry, textMaterial1);
-      text1.position.set(0, value, 0);
-      scene.add(text1);
-
-      let textMaterial2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-      let text2 = new THREE.Mesh(textGeometry, textMaterial2);
-      text2.position.set(0, 0, value);
-      scene.add(text2);
-    }
-  }
-);
+// const graphGeometry = new THREE.BufferGeometry().setFromPoints(points[options.index_variation]);
+// const graph = new THREE.Line(graphGeometry, graphMaterial);
+// scene.add(graph);
 
 
-// const gridHelper = new THREE.GridHelper(30);
-// scene.add(gridHelper);
+
+
+
+
+
+
 
 
 function animate() {
+  box.lookAt(camera.position);
+  if(font) {
+    createText(font);
+  }
+
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
